3c8b62d5bad11e956f5557cdf73d4749faa6ae75
Baptiste Mesta <baptiste.mesta@gmail.com>
2021-07-08 11:33:11 +0200

    mutli with failure

--- src/main/java/mutiny/sample/app/App.java
+++ src/main/java/mutiny/sample/app/App.java
@@ -3,12 +3,24 @@
  */
 package mutiny.sample.app;
 
+import io.smallrye.mutiny.Multi;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Multi.createFrom().items("a", "b", "c")
+                .onItem().transform(s -> {
+            if ("b".equals(s)) {
+                throw new RuntimeException("");
+            } else return s;
+        })
+                .onFailure().recoverWithItem("toto")
+                .onItem().transform(item -> item + " mutiny")
+                .onItem().transform(String::toUpperCase)
+                .subscribe().with(
+                item -> System.out.println(">> " + item));
     }
 }
